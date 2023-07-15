60b41c469424042b3c8c882c35c7224645ca4257
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-07-14 22:33:05 -0400

    feat(fronted): added post modal

---
+++ components/Modal.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+interface ModalProps {
+	isOpen: boolean
+	onClose: () => void
+	children: React.ReactNode
+	addClose?: boolean
+}
+
+export default function Modal({
+	isOpen,
+	onClose,
+	children,
+	addClose,
+}: ModalProps) {
+	return (
+		<Transition.Root show={isOpen} as={Fragment}>
+			<Dialog as='div' className='relative z-50' onClose={onClose}>
+				<Transition.Child
+					as={Fragment}
+					enter='ease-out duration-300'
+					enterFrom='opacity-0'
+					enterTo='opacity-100'
+					leave='ease-in duration-200'
+					leaveFrom='opacity-100'
+					leaveTo='opacity-0'
+				>
+					<div
+						className='fixed inset-0 bg-neutral-800 bg-opacity-75 transition-opacity
+            '
+					/>
+				</Transition.Child>
+
+				<div className='fixed inset-0 z-10 overflow-y-auto'>
+					<div className='flex min-h-full items-center justify-center p-4 text-center sm:p-0'>
+						<Transition.Child
+							as={Fragment}
+							enter='ease-out duration-300'
+							enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+							enterTo='opacity-100 translate-y-0 sm:scale-100'
+							leave='ease-in duration-200'
+							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+						>
+							<Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-black text-white px-4 pb-4pt-5 text-left shadow-xl transition-allw-fullsm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
+								{addClose && (
+									<div className='absolute right-2 top-2'>
+										<button onClick={onClose}>
+											<AiOutlineClose />
+										</button>
+									</div>
+								)}
+								{children}
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition.Root>
+	)
+}
--- components/pages/home/Post.tsx
+++ components/pages/home/Post.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import Modal from '@/components/Modal'
+import clsx from 'clsx'
 import Image from 'next/image'
+import { useState } from 'react'
 import {
 	AiOutlineHeart,
 	AiOutlineMessage,
@@ -16,79 +19,123 @@ interface PostProps {
 	text?: string
 }
 
+const postModalOptions = [
+	{
+		label: 'Report',
+		className: 'text-red-600',
+	},
+	{
+		label: 'Unfollow',
+		className: 'text-red-600',
+	},
+	{
+		label: 'Add to favorites',
+	},
+	{
+		label: 'Go to post',
+	},
+	{
+		label: 'Share to...',
+	},
+	{
+		label: 'Copy link',
+	},
+]
+
 export default function Post({ image, text }: PostProps) {
+	const [modalOpen, setIsModalOpen] = useState(false)
+
 	return (
-		<div className='flex flex-col gap-y-2 w-[80%]'>
-			<div className='flex items-center justify-between w-full'>
-				<div className='flex items-center gap-x-2'>
-					<Image
-						src={'/images/avatar_placeholder.png'}
-						width={20}
-						height={20}
-						alt={'User avatar'}
-						className='rounded-full'
-					/>
-					<div className='flex flex-col items-start justify-start'>
-						<span className='text-sm text-white font-bold'>
-							jhondoe{' '}
-							<span className='text-neutral-600'>
-								• 1h
+		<>
+			<Modal isOpen={modalOpen} onClose={() => setIsModalOpen(false)}>
+				<div className='flex w-full flex-col items-center'>
+					{postModalOptions.map((option, index) => (
+						<span
+							className={clsx(
+								'font-bold my-2 text-xl border-b border-neutral-700 w-full text-center pb-4',
+								option.className && option.className
+							)}
+						>
+							{option.label}
+						</span>
+					))}
+				</div>
+			</Modal>
+			<div className='flex flex-col gap-y-2 w-[80%]'>
+				<div className='flex items-center justify-between w-full'>
+					<div className='flex items-center gap-x-2'>
+						<Image
+							src={'/images/avatar_placeholder.png'}
+							width={20}
+							height={20}
+							alt={'User avatar'}
+							className='rounded-full'
+						/>
+						<div className='flex flex-col items-start justify-start'>
+							<span className='text-sm text-white font-bold'>
+								jhondoe{' '}
+								<span className='text-neutral-600'>
+									• 1h
+								</span>
 							</span>
-						</span>
-						<span className='text-[10px]'>
-							Some extra data
-						</span>
+							<span className='text-[10px]'>
+								Some extra data
+							</span>
+						</div>
 					</div>
-				</div>
-				<HiOutlineDotsHorizontal className='cursor-pointer hover:text-gray-600' />
-			</div>
-			<div className='border border-neutral-600 rounded-sm relative w-full h-[350px] overflow-hidden'>
-				<Image
-					src={image}
-					fill
-					objectFit='cover'
-					alt='Post image'
-					className='hover:scale-105 transition-all cursor-pointer'
-				/>
-				<button className='absolute bottom-5 left-5 p-2 bg-black rounded-full hover:scale-110 transition-all'>
-					<AiOutlineUser />
-				</button>
-			</div>
-			<div className='flex items-center justify-between'>
-				<div className='flex items-center gap-x-5'>
-					<AiOutlineHeart
-						size={30}
-						className='hover:text-neutral-600 transition-all cursor-pointer'
-					/>
-					<AiOutlineMessage
-						size={30}
-						className='hover:text-neutral-600 transition-all cursor-pointer'
-					/>
-					<AiOutlineSend
-						size={30}
-						className='hover:text-neutral-600 transition-all cursor-pointer'
+					<HiOutlineDotsHorizontal
+						className='cursor-pointer hover:text-gray-600'
+						onClick={() => setIsModalOpen(true)}
 					/>
 				</div>
-				<BsBookmark
-					size={30}
-					className='hover:text-neutral-600 transition-all cursor-pointer'
-				/>
+				<div className='border border-neutral-600 rounded-sm relative w-full h-[350px] overflow-hidden'>
+					<Image
+						src={image}
+						fill
+						objectFit='cover'
+						alt='Post image'
+						className='hover:scale-105 transition-all cursor-pointer'
+					/>
+					<button className='absolute bottom-5 left-5 p-2 bg-black rounded-full hover:scale-110 transition-all'>
+						<AiOutlineUser />
+					</button>
+				</div>
+				<div className='flex items-center justify-between'>
+					<div className='flex items-center gap-x-5'>
+						<AiOutlineHeart
+							size={30}
+							className='hover:text-neutral-600 transition-all cursor-pointer'
+						/>
+						<AiOutlineMessage
+							size={30}
+							className='hover:text-neutral-600 transition-all cursor-pointer'
+						/>
+						<AiOutlineSend
+							size={30}
+							className='hover:text-neutral-600 transition-all cursor-pointer'
+						/>
+					</div>
+					<BsBookmark
+						size={30}
+						className='hover:text-neutral-600 transition-all cursor-pointer'
+					/>
+				</div>
+				<span className='flex items-center justify-start font-bold text-sm'>
+					548 likes
+				</span>
+				<span className='text-sm'>{text}</span>
+				<span className='text-xs text-neutral-500 hover:text-neutral-400 cursor-pointer mb-5'>
+					View all 20 comments
+				</span>
+				<div className='border-b border-neutral-700 flex items-center justify-between'>
+					<input
+						type='text'
+						className='bg-transparent focus:outline-none w-full'
+						placeholder='Add a comment'
+					/>
+					<BsEmojiSmile className='text-neutral-400 hover:text-neutral-200 transition-all' />
+				</div>
 			</div>
-			<span className='flex items-center justify-start font-bold text-sm'>
-				548 likes
-			</span>
-			<span className='text-sm'>{text}</span>
-			<span className='text-xs text-neutral-500 hover:text-neutral-400 cursor-pointer mb-5'>
-				View all 20 comments
-			</span>
-			<div className='border-b border-neutral-700 flex items-center justify-between'>
-				<input
-					type='text'
-					className='bg-transparent focus:outline-none w-full'
-					placeholder='Add a comment'
-				/>
-				<BsEmojiSmile className='text-neutral-400 hover:text-neutral-200 transition-all' />
-			</div>
-		</div>
+		</>
 	)
 }
